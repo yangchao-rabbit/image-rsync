d7ba7115d2a1b012ca001db4f35b2905114870e3
yangchaoyi <1767257599@qq.com>
2022-03-02 19:46:02 +0800

    使用sh同步2

--- rsync.sh
+++ rsync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-src_image=("k8s.gcr.io/ingress-nginx/controller:v1.1.1", "k8s.gcr.io/ingress-nginx/kube-webhook-certgen:v1.1.1")
-dest_image=("yangchao1/ingress-nginx-controller:v1.1.1", "yangchao1/ingress-nginx-kube-webhook-certgen:v1.1.1")
+src_image=("k8s.gcr.io/ingress-nginx/controller:v1.1.1" "k8s.gcr.io/ingress-nginx/kube-webhook-certgen:v1.1.1")
+dest_image=("yangchao1/ingress-nginx-controller:v1.1.1" "yangchao1/ingress-nginx-kube-webhook-certgen:v1.1.1")
 
 for i in {0..1} ; do
   docker pull ${src_image[i]}
