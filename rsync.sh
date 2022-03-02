0942ce4798359b98481520d46df4b19864d9e17d
yangchaoyi <1767257599@qq.com>
2022-03-02 19:25:35 +0800

    删除非必要文件

--- rsync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker pull k8s.gcr.io/ingress-nginx/kube-webhook-certgen:v1.1.1
-sleep 2
-docker tag k8s.gcr.io/ingress-nginx/kube-webhook-certgen:v1.1.1 yangchao1/ingress-nginx/kube-webhook-certgen:v1.1.1
-sleep 2
-docker push yangchao1/ingress-nginx/kube-webhook-certgen:v1.1.1
